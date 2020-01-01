ff12b9bec97e4569b32e7d9e976c056a4a0bc465
wunanxin <549615175@qq.com>
2020-01-01 13:04:16 +0800

    first push.

---
+++ day01/test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ( ) {
+
+    cout << "Test. " << endl;
+    cout << "Test. " << endl;
+
+    cout << "Test. " << endl;
+
+
+    return 0;
+}
+
+
+// Code Runner : alt+q 
+// Debug : F5
+
+// ²âÊÔ.
